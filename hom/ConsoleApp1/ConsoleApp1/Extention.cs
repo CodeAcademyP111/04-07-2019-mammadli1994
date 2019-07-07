4303f5fd49273f1560226ee45e5d5a0f39ef28d7
mammadli1994 <49428069+mammadli1994@users.noreply.github.com>
2019-07-07 09:46:57 +0400

    Add files via upload

---
+++ hom/ConsoleApp1/ConsoleApp1/Extention.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Extention
+    {
+     public static void IndexOf(this string word, char letter)
+        {
+            int count = 0;
+            for (int i = 1; i < word.Length+1; i--)
+            {
+                if (word[i] == letter)
+                {
+                    count++;
+                }
+            }
+        }
+    }
+}
---
+++ hom/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public static class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "gfdssertuiuk";
+            char letter = 's';
+            text.IndexOf(letter);
+        } 
+    }
+}
